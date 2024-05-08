99b686e720c20ff302f85fa3e1911898ac190483
oakcrypton <oak@oakton.ru>
2024-05-08 17:56:59 +0500

    dsf'

--- bot.js
+++ bot.js
@@ -1,34 +1,47 @@
-const { Telegraf } = require('telegraf');
 const https = require('https');
 const fs = require('fs');
+const TelegramBot = require('node-telegram-bot-api');
 
 const hostname = 'oakgame.tech';
+const token = '6708244878:AAF9sm6Io9B4dlGtVVv64KxtnLZKcXQ0g8Y';
+
 const options = {
   key: fs.readFileSync('/etc/letsencrypt/live/oakgame.tech/privkey.pem'),
   cert: fs.readFileSync('/etc/letsencrypt/live/oakgame.tech/fullchain.pem')
 };
+
 const port = 443;
 
-const bot = new Telegraf('6708244878:AAF9sm6Io9B4dlGtVVv64KxtnLZKcXQ0g8Y');
-
-bot.start((ctx) => {
-  const fullname = `${ctx.message.from.first_name} ${ctx.message.from.last_name}`;
-  const welcomeMessage = `Приветствую, ${fullname}! Это тестовое приложение игру OAK Clicker! Чтобы запустить игру нажми на кнопку ниже.`;
-  const inlineKeyboard = {
-    inline_keyboard: [
-      [
-        { text: 'Запустить игру', url: 'http://yourgameurl.com' }
-      ]
-    ]
-  };
-
-  ctx.reply(welcomeMessage, { reply_markup: inlineKeyboard });
+const server = https.createServer(options, (req, res) => {
+  if (req.url === '/test') {
+    res.writeHead(200);
+    res.end('Server has been successfully started!\n');
+  } else {
+    res.writeHead(404);
+    res.end('The page was not found!\n');
+  }
 });
-
-const server = https.createServer(options, bot.webhookCallback('/'));
 
 server.listen(port, hostname, () => {
   console.log(`Сервер запущен на https://${hostname}:${port}`);
+  
+  // Запускаем Telegram бот
+  const bot = new TelegramBot(token, { polling: false });
+  
+  // Обработчик команды /start
+  bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+    const fullname = msg.from.first_name + ' ' + msg.from.last_name;
+    
+    // Отправляем приветственное сообщение с кнопкой
+    bot.sendMessage(chatId, `Приветствую, ${fullname}! Это тестовое приложение игру OAK Clicker! Чтобы запустить игру нажми на кнопку ниже.`, {
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: 'Запустить Игру', url: 'https://t.me/oakclickertest_bot/click' }]
+        ]
+      }
+    });
+  });
 });
 
 server.on('error', (error) => {
