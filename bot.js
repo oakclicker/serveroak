fe030e1205bd37c82096bb775ddf26a390183136
oakcrypton <oak@oakton.ru>
2024-05-08 17:38:30 +0500

    fdf

--- bot.js
+++ bot.js
@@ -1,13 +1,15 @@
 const https = require('https');
 const fs = require('fs');
+const Telegraf = require('telegraf');
+const token = '6708244878:AAF9sm6Io9B4dlGtVVv64KxtnLZKcXQ0g8Y';
+
 const hostname = 'oakgame.tech';
+const port = 443;
 
 const options = {
   key: fs.readFileSync('/etc/letsencrypt/live/oakgame.tech/privkey.pem'),
   cert: fs.readFileSync('/etc/letsencrypt/live/oakgame.tech/fullchain.pem')
 };
-
-const port = 443;
 
 const server = https.createServer(options, (req, res) => {
   if (req.url === '/test') {
@@ -19,10 +21,25 @@ const server = https.createServer(options, (req, res) => {
   }
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Сервер запущен на https://${hostname}:${port}`);
+const bot = new Telegraf(token);
+
+bot.start(async (ctx) => {
+  const fullname = ctx.from.first_name + ' ' + ctx.from.last_name;
+  await ctx.replyWithMarkdown(`Приветствую, ${fullname}! Это тестовое приложение игру OAK Clicker! Чтобы запустить игру нажми на кнопку ниже.`, {
+    reply_markup: {
+      inline_keyboard: [[{
+        text: 'Запустить Игру',
+        url: 'https://t.me/oakclickertest_bot/click'
+      }]]
+    }
+  });
 });
 
 server.on('error', (error) => {
   console.error(`Ошибка запуска сервера: ${error}`);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Сервер запущен на https://${hostname}:${port}`);
+  bot.launch();
 });
